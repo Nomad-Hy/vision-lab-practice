5a652b2381c864ee12928fc0684a8f3f7e1c7408
Hoyun choi <hoyunchoe123@gmail.com>
2026-06-22 17:36:47 +0900

    day1 complete!!

--- labs/week01_opencv_basics/day01_image_io_preprocessing/guides/checklist.md
+++ labs/week01_opencv_basics/day01_image_io_preprocessing/guides/checklist.md
@@ -2,56 +2,56 @@
 
 ## Setup
 
-- [ ] 리포 최상위 폴더를 VS Code에서 열었다
-- [ ] `requirements.txt`가 리포 최상위에 있다
-- [ ] 가상환경을 만들었다
-- [ ] 필요한 패키지를 설치했다
+- [o] 리포 최상위 폴더를 VS Code에서 열었다
+- [o] `requirements.txt`가 리포 최상위에 있다
+- [o] 가상환경을 만들었다
+- [o] 필요한 패키지를 설치했다
 
 ## Files
 
-- [ ] Day 1 폴더가 있다
-- [ ] `src/main.py`가 있다
-- [ ] `notes/day01_log.md`가 있다
-- [ ] `guides/concept_guide.md`를 읽었다
-- [ ] `guides/implementation_guide.md`를 읽었다
+- [o] Day 1 폴더가 있다
+- [o] `src/main.py`가 있다
+- [o] `notes/day01_log.md`가 있다
+- [o] `guides/concept_guide.md`를 읽었다
+- [o] `guides/implementation_guide.md`를 읽었다
 
 ## Data
 
-- [ ] `data/raw/sample.jpg`를 넣었다
-- [ ] 이미지 안에 물체와 배경이 있다
-- [ ] 이미지가 너무 단순하지 않다
+- [o] `data/raw/sample.jpg`를 넣었다
+- [o] 이미지 안에 물체와 배경이 있다
+- [o] 이미지가 너무 단순하지 않다
 
 ## Code
 
-- [ ] TODO 1을 채웠다
-- [ ] TODO 2를 채웠다
-- [ ] TODO 3을 채웠다
-- [ ] TODO 4를 채웠다
-- [ ] TODO 5를 채웠다
-- [ ] TODO 6을 채웠다
-- [ ] TODO 7을 채웠다
-- [ ] TODO 8을 채웠다
-- [ ] TODO 9를 채웠다
-- [ ] TODO 10을 채웠다
-- [ ] TODO 11을 채웠다
-- [ ] TODO 12를 채웠다
-- [ ] TODO 13을 채웠다
+- [o] TODO 1을 채웠다
+- [o] TODO 2를 채웠다
+- [o] TODO 3을 채웠다
+- [o] TODO 4를 채웠다
+- [o] TODO 5를 채웠다
+- [o] TODO 6을 채웠다
+- [o] TODO 7을 채웠다
+- [o] TODO 8을 채웠다
+- [o] TODO 9를 채웠다
+- [o] TODO 10을 채웠다
+- [o] TODO 11을 채웠다
+- [o] TODO 12를 채웠다
+- [o] TODO 13을 채웠다
 
 ## Results
 
-- [ ] `grayscale.png`가 생성됐다
-- [ ] `blur.png`가 생성됐다
-- [ ] `edges.png`가 생성됐다
-- [ ] 결과를 직접 눈으로 비교했다
+- [o] `grayscale.png`가 생성됐다
+- [o] `blur.png`가 생성됐다
+- [o] `edges.png`가 생성됐다
+- [o] 결과를 직접 눈으로 비교했다
 
 ## Writing
 
-- [ ] `day01_log.md`를 직접 작성했다
-- [ ] README의 결과 요약을 직접 작성했다
-- [ ] 면접 답변 문장을 직접 작성했다
+- [o] `day01_log.md`를 직접 작성했다
+- [o] README의 결과 요약을 직접 작성했다
+- [o] 면접 답변 문장을 직접 작성했다
 
 ## Git
 
-- [ ] `git status`로 변경사항을 확인했다
-- [ ] commit 했다
-- [ ] push 했다
+- [o] `git status`로 변경사항을 확인했다
+- [o] commit 했다
+- [o] push 했다
--- labs/week01_opencv_basics/day01_image_io_preprocessing/src/main.py
+++ labs/week01_opencv_basics/day01_image_io_preprocessing/src/main.py
@@ -9,7 +9,7 @@ TODO:
 
 from pathlib import Path
 
-import cv2
+import cv2 as cv
 
 
 def main() -> None:
@@ -25,60 +25,69 @@ def main() -> None:
     # Path(__file__).resolve()               -> 절대 경로
     # Path(__file__).resolve().parent        -> src 폴더
     # Path(__file__).resolve().parent.parent -> day01_image_io_preprocessing 폴더
-    project_dir = None
+    project_dir = Path(__file__).resolve( ).parent.parent
 
     # TODO 2:
     # 입력 이미지 경로를 만드세요.
     # 목표:
     # day01_image_io_preprocessing/data/raw/sample.jpg
-    input_path = None
-
+    input_path = project_dir/"data"/"raw"/"jessica-lam-yEF7MFfVbp8-unsplash.jpg"
+    
+    
     # TODO 3:
     # 결과 이미지 저장 폴더 경로를 만드세요.
     # 목표:
     # day01_image_io_preprocessing/outputs/images
-    output_dir = None
+    output_dir = project_dir/"outputs"/"images"
 
     # TODO 4:
     # output_dir 폴더가 없으면 자동으로 만들도록 하세요.
     # 힌트:
     # output_dir.mkdir(parents=True, exist_ok=True)
-
+    output_dir.mkdir(parents=True,exist_ok=True)
+    
+    
     # TODO 5:
     # input_path 위치에 이미지 파일이 없으면 FileNotFoundError를 발생시키세요.
+    if not input_path.exists():
+        raise FileNotFoundError(f"이미지 파일이 없습니다: {input_path}\n 폴더에 파일이 있는지 다시 한번 확인해 주세요.")
+    
 
     # TODO 6:
     # OpenCV로 이미지를 읽으세요.
     # 힌트:
     # cv2.imread(str(input_path))
-    image = None
+    image = cv.imread(str(input_path))
 
     # TODO 7:
     # 이미지가 제대로 읽히지 않았으면 ValueError를 발생시키세요.
     # 힌트:
     # cv2.imread는 실패하면 None을 반환합니다.
+    if image is None:
+        raise ValueError(f"image를 읽지 못하였습니다.")
 
     # TODO 8:
     # image.shape를 출력하세요.
     # 이 값은 이미지의 높이, 너비, 채널 수를 알려줍니다.
-
+    print(f"이미지 크기:{image.shape}")
+    
     # TODO 9:
     # 컬러 이미지를 grayscale 이미지로 바꾸세요.
     # 힌트:
     # cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = None
+    gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
 
     # TODO 10:
     # grayscale 이미지에 Gaussian Blur를 적용하세요.
     # 힌트:
     # cv2.GaussianBlur(gray, (5, 5), 0)
-    blur = None
+    blur = cv.GaussianBlur(gray,(5,5),0)
 
     # TODO 11:
     # blur 이미지에서 Canny Edge를 검출하세요.
     # 힌트:
     # cv2.Canny(blur, 100, 200)
-    edges = None
+    edges = cv.Canny(blur,50,100)
 
     # TODO 12:
     # 결과 이미지 3개를 저장하세요.
@@ -86,10 +95,13 @@ def main() -> None:
     # grayscale.png
     # blur.png
     # edges.png
-
+    success_gray=cv.imwrite(str(output_dir/"grayscale_50_100.png"),gray)
+    success_blur=cv.imwrite(str(output_dir/"blur_50_100.png"),blur)
+    success_edges=cv.imwrite(str(output_dir/"edges_50_100.png"),edges)
     # TODO 13:
     # 저장 완료 메시지를 출력하세요.
-
+    if success_blur and success_edges and success_gray:
+        print("저장 완료!!")
 
 if __name__ == "__main__":
     main()
